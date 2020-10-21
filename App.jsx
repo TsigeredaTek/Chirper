a88c3967484fb8a9462e28fd70960576aeae2f01
TsigeredaTek <70592732+TsigeredaTek@users.noreply.github.com>
2020-10-20 23:03:13 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+
+  return (
+    <div className="container">
+      <header>
+        <h1 className="text-center">Welcome to Chirper!</h1>
+      </header>
+      </div>
+        
+  );
+}
+
+export default App;
---
+++ Chirps.jsx
@@ -0,0 +1,23 @@
+import React, {Fragment} from 'react';
+
+
+const Chirp = (props) => {
+     
+    let chirp = props.chirp;
+    let timeStyle = {
+        position: "relative",
+        bottom: "1em"
+    }
+    return (
+        <Fragment>
+            <li className="list-group-item">
+                <small className="d-block" style={timeStyle}>
+                    {chirp.user} on {chirp.time.toLocaleString()}
+                </small>
+                <span className="ml-5">{chirp.content}</span>
+            </li>
+        </Fragment>
+    );
+}
+
+export default Chirp;
